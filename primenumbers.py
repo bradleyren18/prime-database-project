74159154fcb469c02e8683ae6eda03dfa228cbdc
BradleyRen <148376421+BradleyRen@users.noreply.github.com>
2024-12-29 18:14:21 +0000

    to 50 million!

--- primenumbers.py
+++ primenumbers.py
@@ -3,13 +3,14 @@
 
 def is_prime(number):
     if number % 6 == 1 or number % 6 == 5:
-        if num_of_factors(number) == 2:
-            return True
+        if prp(number) == True:
+            if num_of_factors(number) == 2:
+                return True
     return False
 
 def num_of_factors(number):
     factors = 0
-    for i in range(1, int(number**0.5), 2):
+    for i in range(1, int(number**0.5)+1, 2):
         if number % i == 0:
             factors+=1
     return factors*2
@@ -21,10 +22,35 @@ def primes_below(start, limit):
                 primes_file.write(f"\n{i}")
             # print(i)
 
+def prp(pp):
+    if pp % 2 == 1:
+        npp = factorise(pp-1)
+        s = npp[0]
+        d = npp[1]
+        a = 2
+        bool1 = (a**d) % pp == 1
+        bool2 = (a**d) % pp == -1
+
+        if bool1 == True or bool2 == True:
+            return True
+        return False
+
+
+def factorise(number):
+    power_of_2 = 0
+    remainder = 0
+    while number % 2**power_of_2 == 0:
+        power_of_2 += 1
+    power_of_2 -= 1
+    remainder = number % 2**power_of_2
+
+    return [power_of_2, remainder]
+
 def get_last_prime():
     with open("primes.csv", "r") as primes_file:
-        primes = sorted(primes_file, reverse=True)
-        last_prime = primes[0]
+        # primes = sorted(primes_file)
+        primes = primes_file.readlines()
+        last_prime = list(primes)[-1]
     return int(last_prime)
 
-primes_below(get_last_prime(), 10000000)
+primes_below(get_last_prime(), 50000000)
